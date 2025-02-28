ada5bcf0011a05c0cbccc98063abc18392c6f91f
nasolgabriel <99572309+nasolgabriel@users.noreply.github.com>
2025-02-28 09:44:04 +0800

    replaced content

--- src/components/login/loginContents.ts
+++ src/components/login/loginContents.ts
@@ -6,8 +6,9 @@ type LoginContentType = {
 
 export const loginContent: LoginContentType = {
   welcome: "Welcome to Queueing Master",
-  subSection: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor`,
+  subSection: `This system, created for the Department of Migrant Workers Region IV-A,
+   is a proud initiative of the Laguna State Polytechnic University \u2013 Los Baños Campus,
+    College of Computer Studies, under its Extension and Training Services Unit.`,
   institute: "@ LSPU-LBC-CCS",
 };
 
