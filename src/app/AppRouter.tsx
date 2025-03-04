0aee6fe185c36ff7e80eddf287ae0cc3c1053f18
Gabb <99572309+nasolgabriel@users.noreply.github.com>
2025-03-04 13:06:28 +0800

    Revert "Ward/backend integration"

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,17 +1,26 @@
-"use client";
+// src/app/AppRouter.tsx
+'use client';
 
-import LoginPageBlock from "@/app/(auth)/login/loginPageBlock";
-import QueueDisplay from "@/app/public/queueDisplay/queueDisplay";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import WindowViewBlock from "./protected/windowView/windowViewBlock";
+import LoginPage from '@/components/login/loginPage';
+import QueueDisplay from '@/components/queueDisplay/queueDisplay';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+
+const Home: React.FC = () => (
+  <div>
+    <h1>Home</h1>
+    <nav>
+      <Link to="/queue-display">Go to Queue Display</Link>
+    </nav>
+  </div>
+);
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPageBlock />} />
-        <Route path="/window-view" element={<WindowViewBlock />} />
+        <Route path="/" element={<LoginPage/>} />
         <Route path="/queue-display" element={<QueueDisplay />} />
       </Routes>
     </Router>
