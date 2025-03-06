99b2438705eba54fe005e5b12fc7ee0f5ef6412b
mlyward9 <mlyward9@gmail.com>
2025-03-06 09:35:18 +0800

    backend login api implementation

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+// In routes/api.php
+Route::get('/users', [UserController::class, 'index']);
+
+
+
 
 require __DIR__.'/auth.php';
